ea0e99cec42f0887afdc916acda41aa3ca9e7f54
thuytrangtq22 <thuytrangtq22@gmail.com>
2022-11-01 16:40:30 +0700

    edit show

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,11 @@ use App\Http\Requests;
 
 class UserController extends Controller
 {
-   public function index(Request $request){
-            $search = $request->search;
-           $users = User::where('full_name',  'like', '%' . $search . '%' )->orderBy('id', 'desc')->paginate(20);
-           return view('users.index', compact('users'));
-   }
+    public function index(Request $request)
+    {
+        $search = $request->search;
+        $users = User::where('full_name', 'like', '%' . $search . '%')->orderBy('id', 'desc')->paginate(20);
+        return view('users.index', compact('users'));
+    }
 
 }
