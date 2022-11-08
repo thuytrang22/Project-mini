adb341f28fbb887fb6ad90592a22b3d63c886de3
thuytrangtq22 <thuytrangtq22@gmail.com>
2022-11-08 10:42:54 +0700

    edit pagination

--- resources/js/paginate.js
+++ resources/js/paginate.js
@@ -11,10 +11,18 @@ const range = delta + 4;
 const numberTruncateLeft = curPage - delta;
 const numberTruncateRight = curPage + delta;
 let active = '';
+let renderTwoSide = '';
 for (let pos = 1; pos <= totalPages; pos++) {
     active = pos == curPage ? 'active' : '';
     if (totalPages >= 2 * range - 1 && truncate) {
-        "..."
+        if (
+            numberTruncateLeft > 3 &&
+            numberTruncateRight < totalPages - 3 + 1
+        ) {
+            if (pos >= numberTruncateLeft && pos <= numberTruncateRight) {
+                renderTwoSide += renderPage(pos, active);
+            }
+        }
     } else {
         render += renderPage(pos, active);
     }
