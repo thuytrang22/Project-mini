9948790fa2c6e924498b94ecd64019d1339909b8
thuytrangtq22 <thuytrangtq22@gmail.com>
2022-11-09 10:08:00 +0700

    edit search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,14 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $keywords = $request->keyword;
+        $keywords = trim($request->keyword);
         if ($keywords != '') {
             $users = User::where('full_name', 'like', '%' . $keywords . '%')
                 ->orderBy('id', 'desc')->Paginate(config('constants.PAGINATION'));
-            return view('users.index', compact('users'));
         } else {
             $users = User::orderBy('id', 'desc')->Paginate(config('constants.PAGINATION'));
-            return view('users.index', compact('users'));
+
         }
+        return view('users.index', compact('users'));
     }
 }
