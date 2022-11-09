4845de73f2d70f919b62bfcc7699af453790003d
thuytrangtq22 <thuytrangtq22@gmail.com>
2022-11-09 13:56:02 +0700

    format code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,12 +6,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $search = $request->search;
         $users = User::where('full_name', 'like', '%' . $search . '%')->orderBy('id', 'desc')->paginate(5);
         return view('users.index', compact('users'));
     }
-    public function destroy(User $user){
+
+    public function destroy(User $user)
+    {
         $user->delete();
         return back()->with('success', 'User has been deleted successfully');
     }
